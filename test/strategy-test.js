b47b2c4a6da729484c362f4b47f2b479834bf27f
Jared Hanson <jaredhanson@gmail.com>
2012-04-29 19:21:27 -0700

    Wrap error for informative messages.

--- test/strategy-test.js
+++ test/strategy-test.js
@@ -100,6 +100,9 @@ vows.describe('LinkedInStrategy').addBatch({
       'should error' : function(err, req) {
         assert.isNotNull(err);
       },
+      'should wrap error in InternalOAuthError' : function(err, req) {
+        assert.equal(err.constructor.name, 'InternalOAuthError');
+      },
       'should not load profile' : function(err, profile) {
         assert.isUndefined(profile);
       },
