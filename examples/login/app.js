ff408bb43b33caf9222dd8bc600199590f8e254a
Jake Seip <jake.seip@gmail.com>
2013-06-11 18:14:10 -0700

    fixed example to work with express 3.x

--- examples/login/app.js
+++ examples/login/app.js
@@ -1,11 +1,29 @@
 var express = require('express')
+  , http = require('http')
   , passport = require('passport')
   , util = require('util')
   , LinkedInStrategy = require('passport-linkedin').Strategy;
 
-var LINKEDIN_API_KEY = "--insert-linkedin-api-key-here--"
-var LINKEDIN_SECRET_KEY = "--insert-linkedin-secret-key-here--";
+var app = express();
 
+// configure Express
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.logger());
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session({ secret: 'keyboard cat' }));
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+
+var LINKEDIN_API_KEY = "--insert-linkedin-api-key-here--";
+var LINKEDIN_SECRET_KEY = "--insert-linkedin-secret-key-here--";
 
 // Passport session setup.
 //   To support persistent login sessions, Passport needs to be able to
@@ -35,7 +53,6 @@ passport.use(new LinkedInStrategy({
   function(token, tokenSecret, profile, done) {
     // asynchronous verification, for effect...
     process.nextTick(function () {
-      
       // To keep the example simple, the user's LinkedIn profile is returned to
       // represent the logged-in user.  In a typical application, you would want
       // to associate the LinkedIn account with a user record in your database,
@@ -44,29 +61,6 @@ passport.use(new LinkedInStrategy({
     });
   }
 ));
-
-
-
-
-var app = express.createServer();
-
-// configure Express
-app.configure(function() {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.logger());
-  app.use(express.cookieParser());
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.session({ secret: 'keyboard cat' }));
-  // Initialize Passport!  Also use passport.session() middleware, to support
-  // persistent login sessions (recommended).
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
 
 app.get('/', function(req, res){
   res.render('index', { user: req.user });
@@ -97,7 +91,7 @@ app.get('/auth/linkedin',
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
-app.get('/auth/linkedin/callback', 
+app.get('/auth/linkedin/callback',
   passport.authenticate('linkedin', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
@@ -108,7 +102,9 @@ app.get('/logout', function(req, res){
   res.redirect('/');
 });
 
-app.listen(3000);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
 
 // Simple route middleware to ensure user is authenticated.
@@ -118,5 +114,5 @@ app.listen(3000);
 //   login page.
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
+  res.redirect('/login');
 }
